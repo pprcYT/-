b25afa8a4b36b585e13ee070d3621e49e4abcaf8
해티 <admin@hatty163.kr>
2020-04-14 20:09:05 +0900

    20.4.10

---
+++ Gruntfile.js
@@ -0,0 +1,78 @@
+const LICENSE = [
+	"Rule the Words!, AlphaKKuTu Online. ⓒ 2017-2019 AlphaKKuTu. All Rights Reserved.",
+	"This program is distributed by terms of GNU Affero General Public License.",
+	"For more information, please check: <https://www.gnu.org/licenses/agpl-3.0.html>."
+].join('\n');
+
+var File = require('fs');
+
+const LIST = [
+	"global",
+	
+	"in_login",
+	"in_game_kkutu",
+	"in_game_kkutu_help",
+	"in_game_kkutu_user",
+	"in_admin",
+	"in_portal",
+	"in_loginfail",
+	
+	"kpanel/in_admin",
+	"kpanel/in_daneo",
+	"kpanel/in_user"
+];
+const KKUTU_LIST = [
+	"web/lib/kkutu/head.js",
+	"web/lib/kkutu/ready.js",
+	"web/lib/kkutu/rule_classic.js",
+	"web/lib/kkutu/rule_jaqwi.js",
+	"web/lib/kkutu/rule_crossword.js",
+	"web/lib/kkutu/rule_typing.js",
+	"web/lib/kkutu/rule_hunmin.js",
+	"web/lib/kkutu/rule_daneo.js",
+	"web/lib/kkutu/rule_drawing.js",
+	"web/lib/kkutu/rule_sock.js",
+	"web/lib/kkutu/body.js",
+	"web/lib/kkutu/tail.js"
+];
+
+module.exports = function(grunt){
+	var i, files = {}, cons = {};
+	var KKUTU = "web/public/js/in_game_kkutu.min.js";
+	
+	for(i in LIST){
+		files["web/public/js/"+LIST[i]+".min.js"] = "web/lib/"+LIST[i]+".js";
+	}
+	files[KKUTU] = KKUTU_LIST;
+	
+	grunt.initConfig({
+		uglify: {
+			options: {
+				banner: "/**\n" + LICENSE + "\n*/\n\n"
+			},
+			build: {
+				files: files
+			}
+		},
+		concat: {
+			basic: {
+				src: KKUTU_LIST,
+				dest: "Web/lib/in_game_kkutu.js"
+			}
+		}
+	});
+	grunt.loadNpmTasks('grunt-contrib-uglify-es');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	
+	grunt.registerTask('default', ['concat', 'uglify']);
+	grunt.registerTask('pack', 'Log', function(){
+		var done = this.async();
+		var url = __dirname + "/" + KKUTU;
+		
+		File.readFile(url, function(err, res){
+			File.writeFile(url, "(function(){" + res.toString() + "})();", function(err, res){
+				done();
+			});
+		})
+	});
+};
---
+++ config.js
@@ -0,0 +1,21 @@
+var JavaScriptObfuscator = require('javascript-obfuscator');
+var obfuscationResult = JavaScriptObfuscator.obfuscate(
+    `
+        (function(){
+            var variable1 = '5' - 3;
+            var variable2 = '5' + 3;
+            var variable3 = '5' + - '2';
+            var variable4 = ['10','10','10','10','10'].map(parseInt);
+            var variable5 = 'foo ' + 1 + 1;
+            console.log(variable1);
+            console.log(variable2);
+            console.log(variable3);
+            console.log(variable4);
+            console.log(variable5);
+        })();
+    `,
+    {
+        compact: false,
+        controlFlowFlattening: true
+    }
+);
